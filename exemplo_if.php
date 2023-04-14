8c0896a1b6e9df33a0bfd23b52574533732b5232
JOAQUIM PEDRO KLIMA SAGIONETI <joaquim.2944@aluno.pr.senac.br>
2023-04-14 08:00:10 -0300

    sua hora chegou

---
+++ exemplo_if.php
@@ -0,0 +1,12 @@
+<?php
+    $idade = 30;
+
+
+     if ($idade <= 17){
+        echo("sua idade é menor de 18 anos");
+     }elseif($idade >18){
+        echo("sua idade é maior de 18 anos");
+        }else{
+            echo("Não corresponde a sua idade");
+        }
+?>
